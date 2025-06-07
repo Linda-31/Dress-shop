1e93555ca11063c25401ea3b094c5fa7bd1181a8
Linda3104 <lindaqueentin701@gmail.com>
2025-06-07 12:25:18 +0530

    second change

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     <Router>
     <Header /> 
        <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/Signup" element={<Signup />} />
          <Route path="/login" element={<Login />} />
          <Route path="/Shop" element={<Shop />} />
