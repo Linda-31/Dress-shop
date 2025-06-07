fdf94036698f589a434dbcff243282f3b36747e3
Linda3104 <lindaqueentin701@gmail.com>
2025-06-07 13:27:55 +0530

    third changes

--- src/Pages/Shop.js
+++ src/Pages/Shop.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import "../Styles/style.css";
+// import axios from 'axios';
+// import { useEffect, useState } from 'react';
+
 function Shop() {
+    // const [products, setProducts] = useState([]);
+
+//     useEffect(() => {
+//     axios.get("/data.json")
+//       .then((response) => {
+//         setProducts(response.data);
+//       })
+//       .catch((error) => {
+//         console.error("Error fetching product data:", error);
+//       });
+//   }, []);
     return(
- <div>
-    
- </div>
-    );
+//    <div className="card" style={{ width: '18rem' }}>
+//       <img src={products.imageUrl} className="card-img-top" alt={products.name} />
+//       <div className="card-body">
+//         <h5 className="card-title">{products.name}</h5>
+//         <p className="card-text">Price: Rs {products.price.toFixed(2)}</p>
+//         <button className="btn btn-primary">
+//           Add to Cart
+//         </button>
+//       </div>
+//     </div>
+<div>
+
+</div>
+      );
 }
 export default Shop;
